9dd62ae391e2e6e54e1860faecbf02819d03ac53
VladVaskov <165757637+VladVaskov@users.noreply.github.com>
2024-06-13 14:19:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const a = document.getElementById("a")
+const b = document.getElementById("b")
+const c = document.getElementById("c")
+
+a.addEventListener("click", () => {
+  alert("Неправильна відповідь!")
+})
+
+b.addEventListener("click", () => {
+  alert("Неправильна відповідь!")
+})
+
+c.addEventListener("click", () => {
+  alert("Правильно!")
+})
